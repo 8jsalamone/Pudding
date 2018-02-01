f02bbfbca02e7f20ca32858f89e4bac8d5d5c0e7
angery caillou <31480869+8jsalamone@users.noreply.github.com>
2018-02-01 11:57:35 -0600

    Update Pudding.js

--- Pudding.js
+++ Pudding.js
@@ -1,1 +1,6 @@
-
+function pudding(meat) {
+  If(meat > 1){
+    Console.log("you can have pudding")} 
+  Else{console.log("No pudding! How can you eat your pudding if you haven't eat your meat!!") 
+      } 
+  Pudding(5)
